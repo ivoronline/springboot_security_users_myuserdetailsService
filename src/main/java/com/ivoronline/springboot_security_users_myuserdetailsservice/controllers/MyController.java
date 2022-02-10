e70196b00adf4655f4b180641fe71e63e68e26f3
ivoronline <ivoronline@gmail.com>
2022-02-10 18:55:17 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_security_users_myuserdetailsservice/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_users_myuserdetailsservice/controllers/MyController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
   //==============================================================================
-  // HELLO (Secured Resource)
+  // HELLO
   //==============================================================================
   @Secured("ROLE_USER")
   @RequestMapping("Hello")
